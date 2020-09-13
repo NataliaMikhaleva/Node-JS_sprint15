6e66eba0bccaae4dac3efd328f69b5cb1414804c
Natalia <mihaleva.natalya2018@yandex.ru>
2020-09-13 19:56:38 +0300

    conflict

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 const { PORT = 3000, BASE_PATH } = process.env;
-app.use(express.static((path.join(__dirname, 'public'))));
 
 const { requestLogger, errorLogger } = require('./middlewares/Logger');
 
